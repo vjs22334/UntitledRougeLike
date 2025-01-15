79c65baef1016ce141dc7d140cd04ec948fc3386
jeyasurya <vjs122334@gmail.com>
2025-01-16 00:09:46 +0530

    changed player blueprints to top down controls

--- Source/UntitledRougelike/UntitledRougelikeCharacter.cpp
+++ Source/UntitledRougelike/UntitledRougelikeCharacter.cpp
@@ -95,19 +95,9 @@ void AUntitledRougelikeCharacter::Move(const FInputActionValue& Value)
 
 	if (Controller != nullptr)
 	{
-		// find out which way is forward
-		const FRotator Rotation = Controller->GetControlRotation();
-		const FRotator YawRotation(0, Rotation.Yaw, 0);
-
-		// get forward vector
-		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
-	
-		// get right vector 
-		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
-
 		// add movement 
-		AddMovementInput(ForwardDirection, MovementVector.Y);
-		AddMovementInput(RightDirection, MovementVector.X);
+		AddMovementInput(FVector::UnitX(), MovementVector.Y);
+		AddMovementInput(FVector::UnitY(), MovementVector.X);
 	}
 }
 
